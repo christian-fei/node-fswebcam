ea129f7374a00b723001fda88a292e32161090fe
Christian Fei <crifei93@gmail.com>
2019-02-23 19:32:25 +0100

    update author

--- command-for.js
+++ command-for.js
@@ -1,6 +1,6 @@
 module.exports = function (args = {}) {
   return Object.keys(args)
-    .reduce((commandString, key, i, object) => {
+    .reduce((commandString, key) => {
       const value = args[key]
       return commandString + ` --${key} ${value}`
     }, 'fswebcam')
